43160762ff982b06c112a9e7a4dfa22d15488259
mrvauxs <mrvauxs@gmail.com>
2025-12-15 09:52:11 +0100

    vault backup: 2025-12-15 09:52:11

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -151,7 +151,7 @@ async function renderGraph(graph: HTMLElement, fullSlug: FullSlug) {
       id: url,
       text,
       tags: data.get(url)?.tags ?? [],
-      color: data.get(url)?.content
+      color: data.get(url)?.content.match("color")[0]
     }
   })
   const graphData: { nodes: NodeData[]; links: LinkData[] } = {
